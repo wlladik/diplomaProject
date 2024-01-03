938030467b2d63950282fbda906b8f7016fb1298
Vlad Lysykh <vladlysykh7@gamil.com>
2024-01-03 18:54:19 +0100

    add dyplomas project on github

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -103,6 +103,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'vladlysykh@gmail.com'
+EMAIL_HOST_PASSWORD = 'zjirwlvhpndzixdb'
+
 
 STATIC_URL = '/static/'
 
